7dbc94eb0a490cd44ceaca55b0851f82afa92f82
Rshei <118082280+Rshei@users.noreply.github.com>
2024-06-02 11:27:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,6 +102,7 @@ if not st.session_state['logged_in']:
         if user:
             user_doc = db.collection('users').document(user['localId']).get()
             user_data = user_doc.to_dict()
+            st.write("Retrieved User Data:", user_data) 
             user_full_name = user_data.get("full_name", "Unknown")
             st.session_state['logged_in'] = True
             st.session_state['user'] = user
